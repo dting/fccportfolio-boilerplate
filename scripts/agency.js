562c1778017c8996cfe4deb62396d3779d6ce9ce
Dennis Ting <dennisting@gmail.com>
2015-08-02 19:46:26 -0700

    Distribution Commit

---
+++ scripts/agency.js
@@ -0,0 +1,1 @@
+$(function(){$("a.page-scroll").bind("click",function(t){var a=$(this);$("html, body").stop().animate({scrollTop:$(a.attr("href")).offset().top},1500,"easeInOutExpo"),t.preventDefault()})}),$("body").scrollspy({target:".navbar-fixed-top"}),$(".navbar-collapse ul li a").click(function(){$(".navbar-toggle:visible").click()});
---
+++ scripts/cbpAnimatedHeader.js
@@ -0,0 +1,1 @@
+var cbpAnimatedHeader=function(){function n(){window.addEventListener("scroll",function(n){o||(o=!0,setTimeout(e,250))},!1)}function e(){var n=t();n>=c?classie.add(r,"navbar-shrink"):classie.remove(r,"navbar-shrink"),o=!1}function t(){return window.pageYOffset||a.scrollTop}var a=document.documentElement,r=document.querySelector(".navbar-default"),o=!1,c=300;n()}();
